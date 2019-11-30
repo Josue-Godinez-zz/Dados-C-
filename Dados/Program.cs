6d6c6d16a41488d610185a6a6b896baf38377414
ShuAkihiro <josuegod1998@gmail.com>
2019-11-30 10:57:08 -0600

    Inicializacion de Proyecto Dados

---
+++ Dados/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
